c24b5fd8e395e5b76fedded0d4321a8d6f6a9d62
Dyaln K Claytor <32943321+DClayotr@users.noreply.github.com>
2021-10-05 13:52:12 -0400

    Delete app.java

--- app.java
+++
@@ -1,8 +0,0 @@
-public class app {
-    public static void main(String[] args) {
-        Model md = new Model();
-        Controller cntl = new Controller(md);
-        View vw = new View(cntl);
-        vw.TicketPrompt();
-    }
-}
